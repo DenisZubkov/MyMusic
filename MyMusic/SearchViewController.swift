674fe298ca8d2696798936855260aa5ac7a0d170
Denis Zubkov <denzu@mac.com>
2020-02-28 19:23:14 +0300

    Lesson 03

--- MyMusic/SearchViewController.swift
+++ MyMusic/SearchViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    let tracks = [TrackModel(trackName: "Gruppa Krovi", artistName: "Kino"),
+                 TrackModel(trackName: "Serebro", artistName: "Bi-2")]
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSearchBar()
+        
+        
         view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        
     }
     
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon")
+        return cell
+    }
+    
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
