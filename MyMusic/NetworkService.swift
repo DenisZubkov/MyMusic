664718373b4292674f427bc16222c06b444e5873
Denis Zubkov <denzu@mac.com>
2020-03-01 19:12:49 +0300

    Lesson 06

---
+++ MyMusic/NetworkService.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkService.swift
+//  MyMusic
+//
+//  Created by Dennis Zubkoff on 01.03.2020.
+//  Copyright © 2020 Denis Zubkov. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class NetworkService {
+    
+    func fetchTracks(searchText: String, completion: @escaping (SearchResponse?) -> Void) {
+        let url = "https://itunes.apple.com/search"
+        let parameters = ["term" : "\(searchText)",
+            "limit" : "10",
+            "media" : "music",]
+        
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            
+            if let error = dataResponse.error {
+                print("Error request: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                print("objects: ", objects)
+                completion(objects)
+            } catch let jsonError{
+                print("Failed to decode JSON ", jsonError)
+                completion(nil)
+            }
+
+        }
+        
+    }
+    
+}
--- MyMusic/SearchViewController.swift
+++ MyMusic/SearchViewController.swift
@@ -18,6 +18,8 @@ class SearchViewController: UITableViewController {
     
     var tracks = [Track]()
     let searchController = UISearchController(searchResultsController: nil)
+    
+    var networkService = NetworkService()
     private var timer: Timer?
     
     override func viewDidLoad() {
@@ -61,30 +63,9 @@ extension SearchViewController: UISearchBarDelegate {
         
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
-            let url = "https://itunes.apple.com/search"
-            let parameters = ["term" : "\(searchText)",
-                              "limit" : "10"]
-            
-            AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
-                
-                if let error = dataResponse.error {
-                    print("Error request: \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let data = dataResponse.data else { return }
-                
-                let decoder = JSONDecoder()
-                do {
-                    let objects = try decoder.decode(SearchResponse.self, from: data)
-                    print("objects: ", objects)
-                    self.tracks = objects.results
-                    self.tableView.reloadData()
-                } catch let jsonError{
-                    print("Failed to decode JSON ", jsonError)
-                }
-//                let someString = String(data: data, encoding: .utf8)
-//                print(someString ?? "")
+            self.networkService.fetchTracks(searchText: searchText) { [weak self] (searchResults) in
+                self?.tracks = searchResults?.results ?? []
+                self?.tableView.reloadData()
             }
         })
         
