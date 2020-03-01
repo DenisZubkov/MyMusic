3842c3a309a0cb0723852b5f89aa453bd3b11502
Denis Zubkov <denzu@mac.com>
2020-03-01 14:57:00 +0300

    Lesson 04

--- MyMusic/SearchViewController.swift
+++ MyMusic/SearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName: String
@@ -57,6 +58,17 @@ extension SearchViewController: UISearchBarDelegate {
     
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData { (dataResponse) in
+            if let error = dataResponse.error {
+                print("Error request: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+        }
     }
 }
