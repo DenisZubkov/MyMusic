2419afab2335b1d2f5249a4aed55ee0087c87e8f
Denis Zubkov <denzu@mac.com>
2020-03-01 22:50:47 +0300

    Lesson 09

--- MyMusic/Search/SearchViewController.swift
+++ MyMusic/Search/SearchViewController.swift
@@ -18,6 +18,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     var router: (NSObjectProtocol & SearchRoutingLogic)?
     
     let searchController = UISearchController(searchResultsController: nil)
+    private var searchViewModel = SearchViewModel.init(cells: [])
+    private var timer: Timer?
     
     @IBOutlet weak var table: UITableView!
     
@@ -64,6 +66,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     private func setupSearchBar() {
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        
+        searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
     }
     
@@ -76,8 +80,10 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
        
         case .some:
             print("viewController .some")
-        case .displayTracks:
+        case .displayTracks(let searchViewModel):
             print("viewController .displayTrack")
+            self.searchViewModel = searchViewModel
+            table.reloadData()
        
         }
     }
@@ -87,12 +93,16 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return searchViewModel.cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "IndexPath: \(indexPath)"
+        let cellViewModel = searchViewModel.cells[indexPath.row]
+        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon")
+
         return cell
     }
     
@@ -105,9 +115,11 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
-        print(searchText)
-        interactor?.makeRequest(request: Search.Model.Request.RequestType.some)
-        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            print(searchText)
+            self.interactor?.makeRequest(request: Search.Model.Request.RequestType.getTracks(searchTerm: searchText))
+        })
         
     }
 }
